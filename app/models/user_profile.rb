1251962b8b3466f6d7f5b6c80d44d7120193c528
cis <cis@CISM332.(none)>
2013-03-26 15:51:08 +0530

    updates_of_story_http://rkelsch.com/dev/Nishant-3-25.html

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,6 +1,9 @@
 class UserProfile < ActiveRecord::Base
   attr_accessible :city, :county, :country, :first_name, :last_name, :latitude, :longitude, :physical_address, :postal_zip_code, :state, :unit_no
   attr_accessible :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar,
+   :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
+   :url =>"/images/avatar/:id/:style/:filename",
+   :default_url => "/images/:style/missing.png"
   belongs_to :user
 end
