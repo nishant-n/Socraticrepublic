e3120c6a1b359dfc5235edc18c11560219c8bdb3
cis <cis@CISM380.(none)>
2013-04-03 15:54:04 +0530

    changes of 3rd april

--- db/migrate/20130403073300_remove_user_id_from_declarations.rb
+++
@@ -1,8 +0,0 @@
-class RemoveUserIdFromDeclarations < ActiveRecord::Migration
-  def self.up	
-   	remove_column :declarations ,:user_id  	
-  end
-  def self.down
-   	add_column :declarations ,:user_id ,:integer
-  end
-end
